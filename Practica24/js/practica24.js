a705152eef948ce44037710c9ebc7aeb472b2637
LivMaster13 <livier.orozco@uabc.edu.mx>
2023-10-03 18:09:55 -0700

    Practica 24 calculadora basica utilizando javascript con declaracion variables var

---
+++ Practica24/js/practica24.js
@@ -0,0 +1,40 @@
+// Ejemplo var numero1 = parseInt(prompt("Ingrese el primer numero: "));
+// var numero2 = parseInt(prompt("Ingrese el segundo numero: "));
+// var resultado = numero1 + numero2;
+
+// alert(resultado);
+
+// var mensaje = "Bienvenido a nuestro sitio web";
+// var texto_1 = " texto comillas dobles";
+// var texto_2 = ' texto comillas simples...';
+
+// alert(mensaje + texto_1 + texto_2);
+
+// var clienteRegistrado = false;
+// var ivaIncluido = true;
+
+// alert(clienteRegistrado);
+
+// var array=['Tijuana','Rosarito','Ensenada','Mexicali'];
+// alert(array);
+
+// alert(array[3]);
+
+var numero1 = parseInt(prompt("Ingrese el primer numero: "));
+var numero2 = parseInt(prompt("Ingrese el segundo numero: "));
+
+var suma = numero1 + numero2;
+var resta = numero1 - numero2;
+var multi = numero1 * numero2;
+var divi = numero1 / numero2;
+
+alert(
+  "La suma de los numeros es: " +
+    suma +
+    " \nLa resta de los numeros es: " +
+    resta +
+    " \nLa multiplicacion de los numeros es: " +
+    multi +
+    " \nLa division de los numeros es: " +
+    divi
+);
