ccadad43c4c30fa8c03297654da4c958c4022b2a
LivMaster13 <livier.orozco@uabc.edu.mx>
2023-10-10 22:37:04 -0700

    ciclo for para saludo

--- Practica29/js/forholasaludo.js
+++ Practica29/js/forholasaludo.js
@@ -1,9 +1,9 @@
-for (var i = 1; i <= 7; i++) {
-  if (i === 4) {
+for (var i = 0; i < 8; i++) {
+  if (i == 3) {
     alert("Soy el 4to");
-  }
-  alert("Hola");
-  if (i === 7) {
+  } else if (i == 7) {
     alert("Acabamos de saludar");
+  } else {
+    alert("Hola número " + (i + 1));
   }
 }
